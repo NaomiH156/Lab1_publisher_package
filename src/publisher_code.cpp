690459cba296d8311e4254aefd5f7fb057f3797b
Naomi Hourihane <jdh156@case.edu>
2019-09-10 16:23:24 -0400

    Naomi Hourihane lab 1 publisher package FINAl

--- src/publisher_code.cpp
+++ src/publisher_code.cpp
@@ -1,12 +1,13 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "publisher_package/topic.h"
 #include <sstream>
 
 int main ( int argc , char ** argv )
 {
 ros :: init ( argc , argv , "publisher_node" );
 ros :: NodeHandle n ;
-ros :: Publisher chatter_pub = n . advertise < std_msgs :: String >( "chatter" , 1000 );
+ros :: Publisher chatter_pub = n . advertise < std_msgs :: String >( "topic" , 1000 );
 ros :: Rate loop_rate (1);
 int count = 0 ;
 while ( ros :: ok ())
